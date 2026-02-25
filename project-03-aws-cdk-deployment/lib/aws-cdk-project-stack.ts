0656f642073d68765db9a0d19dda27acf783d004
sudharsanbaskaran09-eng <sudharsanbaskaran09@gmail.com>
2026-02-25 12:41:34 +0530

    Update aws-cdk-project-stack.ts

--- project-03-aws-cdk-deployment/lib/aws-cdk-project-stack.ts
+++ project-03-aws-cdk-deployment/lib/aws-cdk-project-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class AwsCdkProjectStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -15,17 +16,25 @@ export class AwsCdkProjectStack extends Stack {
     });
 
     // Lambda Function
-    new lambda.Function(this, 'MyCdkLambda', {
+    const myLambda = new lambda.Function(this, 'MyCdkLambda', {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'index.handler',
       code: lambda.Code.fromInline(`
         exports.handler = async function(event) {
           return {
             statusCode: 200,
-            body: "Hello from CDK Lambda"
+            body: JSON.stringify({
+              message: "Hello from AWS CDK Lambda via API Gateway"
+            })
           };
         };
       `),
+    });
+
+    // API Gateway
+    new apigateway.LambdaRestApi(this, 'MyCdkApi', {
+      handler: myLambda,
+      proxy: true,
     });
   }
 }
