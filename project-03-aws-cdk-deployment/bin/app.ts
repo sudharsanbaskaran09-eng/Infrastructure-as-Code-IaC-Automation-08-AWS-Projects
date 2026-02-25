25714a7433fab7c54729ca1b2397ea98455fc609
sudharsanbaskaran09-eng <sudharsanbaskaran09@gmail.com>
2026-02-25 12:40:52 +0530

    Update app.ts

--- project-03-aws-cdk-deployment/bin/app.ts
+++ project-03-aws-cdk-deployment/bin/app.ts
@@ -3,4 +3,5 @@ import * as cdk from 'aws-cdk-lib';
 import { AwsCdkProjectStack } from '../lib/aws-cdk-project-stack';
 
 const app = new cdk.App();
+
 new AwsCdkProjectStack(app, 'AwsCdkProjectStack');
